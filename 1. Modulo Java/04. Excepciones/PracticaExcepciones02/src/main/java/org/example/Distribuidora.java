f623be52313754523ed116de8e47ee32fd0b8701
LucaBeltramini <beltramini19@gmail.com>
2024-02-13 13:15:41 -0300

    Excepciones

---
+++ 1. Modulo Java/04. Excepciones/PracticaExcepciones02/src/main/java/org/example/Distribuidora.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Distribuidora {
+    public static void main(String[] args) {
+        ArrayList<Producto> lista = new ArrayList();
+        //Productos
+        lista.add(new Producto("Escoba", 3180));
+        lista.add(new Producto("Detergente", 1650));
+        lista.add(new Producto("Plato", 2800));
+        lista.add(new Producto("Botella", 1500));
+        lista.add(new Producto("Cuchiclo", 1300));
+        //Perecederos
+        lista.add(new Perecedero("Leche", 920, 50));
+        lista.add(new Perecedero("Manteca", 1300, 3));
+        lista.add(new Perecedero("Yogurt", 800, 2));
+        lista.add(new Perecedero("Queso", 3599, 50));
+        //NoPerecederos
+        lista.add(new NoPerecedero("Fideos", 920, "Pasta"));
+        lista.add(new NoPerecedero("Arroz", 920, "Conserva"));
+        lista.add(new NoPerecedero("Atun", 920, "Enlatado"));
+        lista.add(new NoPerecedero("Garbanzos", 920, "Enlatado"));
+        lista.add(new NoPerecedero("Arbejas", 920, "Enlatado"));
+        double sumaProductoGeneral = 0;
+        double sumaPerecedero = 0;
+        double sumaNoPerecedero = 0;
+        for(Producto prod : lista){
+            if(prod instanceof Perecedero){
+                sumaPerecedero += prod.calcular(1);
+            }
+            else if(prod instanceof NoPerecedero){
+                sumaNoPerecedero += prod.calcular(1);
+            }
+            else {
+                sumaProductoGeneral += prod.calcular(1);
+            }
+        }
+
+        System.out.println("El precio de los productos generales es: " + sumaProductoGeneral);
+        System.out.println("El precio de los productos Perecederos es: " + sumaPerecedero);
+        System.out.println("El precio de los productos No Perecederos es: " + sumaNoPerecedero);
+
+    }
+}
