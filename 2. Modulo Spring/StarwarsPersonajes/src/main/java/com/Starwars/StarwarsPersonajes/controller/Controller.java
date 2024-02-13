52ffd95a8bbd3c6fc8c89d4a3259fa3dfc871b97
Emmanuel Gomez Diaz <emfegodi@gmail.com>
2024-02-13 16:16:16 -0500

    Modulo spring ejercicio 4

---
+++ 2. Modulo Spring/StarwarsPersonajes/src/main/java/com/Starwars/StarwarsPersonajes/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.Starwars.StarwarsPersonajes.controller;
+
+import com.Starwars.StarwarsPersonajes.dto.PersonajeDTO;
+import com.Starwars.StarwarsPersonajes.service.StarwarsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+    StarwarsService starwarsService = new StarwarsService();
+
+    @GetMapping("/findCharacters/{name}")
+    public ResponseEntity<List<PersonajeDTO>> findCharacters(@PathVariable String name){
+        if(!(starwarsService.findCharacters(name).isEmpty()))
+            return new ResponseEntity<>(starwarsService.findCharacters(name), HttpStatus.OK);
+
+        return  new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+}
