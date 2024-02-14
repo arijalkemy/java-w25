c9cf8b1b936e942665bb8413596ea1c233189a2a
asturba2000 <agustin.sturba@mercadolibre.com>
2024-02-14 11:07:43 -0300

    JavaP1Vivo: Ejercicio de Java, donde se busca las ciudades con mayor y menor temperatura. PooP1Vivo: Ejercicio de Java, donde se busca el imc del objeto persona.

---
+++ 1. Modulo Java/PooP1Vivo/src/Main.java
@@ -0,0 +1,11 @@
+import persona.Persona;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        Persona persona1 = new Persona("agustin",19,"43030814");
+        Persona persona2 = new Persona("agustin",19,"43030814",69,170);
+        System.out.println(persona2);
+
+    }
+}
