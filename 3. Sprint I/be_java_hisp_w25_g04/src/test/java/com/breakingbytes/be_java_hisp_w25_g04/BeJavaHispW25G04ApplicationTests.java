c8db6a143ad10413775cdb03ac99dda67c6cf194
laraaraceli <you@example.com>
2024-02-20 14:57:38 -0300

    add group project

---
+++ 3. Sprint I/be_java_hisp_w25_g04/src/test/java/com/breakingbytes/be_java_hisp_w25_g04/BeJavaHispW25G04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakingbytes.be_java_hisp_w25_g04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW25G04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
