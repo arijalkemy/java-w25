81e3f3dcb44f7f4af91a8e5294dce1cc5687e226
Arrigoz <arrigo.zanabonidibatista@mercadolibre.com>
2024-02-20 17:13:25 -0300

    fix controller y userRepo

--- 3. Sprint I/be_java_hisp_w25_g02_zanaboni/src/main/java/com/be_java_hisp_w25_g02_zanaboni/controller/UserController.java
+++ 3. Sprint I/be_java_hisp_w25_g02_zanaboni/src/main/java/com/be_java_hisp_w25_g02_zanaboni/controller/UserController.java
@@ -7,11 +7,9 @@ import com.be_java_hisp_w25_g02_zanaboni.service.IUserService;
 import com.be_java_hisp_w25_g02_zanaboni.service.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class UserController {
     private final IUserService userService;
 
--- 3. Sprint I/be_java_hisp_w25_g02_zanaboni/src/main/java/com/be_java_hisp_w25_g02_zanaboni/repository/UserRepositoryImpl.java
+++ 3. Sprint I/be_java_hisp_w25_g02_zanaboni/src/main/java/com/be_java_hisp_w25_g02_zanaboni/repository/UserRepositoryImpl.java
@@ -14,31 +14,18 @@ public class UserRepositoryImpl implements IUserRepository{
         loadUsers();
     }
     private void loadUsers(){
-        userList = new ArrayList<>(List.of(
-                new User(1, "Javier", false, new ArrayList<>(List.of(
-                        7,9
-
-                )), new ArrayList<>()),
-                new User(2, "NA", false, new ArrayList<>(), new ArrayList<>()),
-                new User(3, "Martin", false, new ArrayList<>(), new ArrayList<>()),
-                new User(7, "Maria", true, new ArrayList<>(),
-
-                        new ArrayList<>(List.of(1)
-                        )),
-                new User(8, "Marcos", false, new ArrayList<>(), new ArrayList<>(
-
-                )),
-                new User(9, "Malena", true, new ArrayList<>(), new ArrayList<>(
-                        List.of(1, 3, 7)
-                )),
-                new User(10, "JoseMaria", true, new ArrayList<>(), new ArrayList<>(
-                        List.of(1)
-                )),
-                new User(11, "Pedro Gomez", true, new ArrayList<>(), new ArrayList<>())
-
-        ));
+        userList =
+          new ArrayList<>(List.of(
+              new User(1, "Arrigo", false, new ArrayList<>(List.of(7,9)),new ArrayList<>()),
+              new User(2, "Beatriz", false, new ArrayList<>(), new ArrayList<>()),
+              new User(3, "Martin", false, new ArrayList<>(), new ArrayList<>()),
+              new User(4, "Erling", true, new ArrayList<>(), new ArrayList<>(List.of(1))),
+              new User(8, "Marcos", false, new ArrayList<>(), new ArrayList<>()),
+              new User(9, "Milena", true, new ArrayList<>(), new ArrayList<>(List.of(1, 3, 7))),
+              new User(10, "JoseMaria", true, new ArrayList<>(), new ArrayList<>(List.of(1))),
+              new User(11, "Pedro Gomez", true, new ArrayList<>(), new ArrayList<>())
+          ));
     }
-
 
     @Override
     public Optional<User> findById(Integer id) {
