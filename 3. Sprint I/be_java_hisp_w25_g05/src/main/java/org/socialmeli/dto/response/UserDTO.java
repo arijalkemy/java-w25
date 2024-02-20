05c9d64cd4a6a5d2ec98ba5fdd274e534c719406
osjuarez <oscar.juarez@mercadolibre.com>
2024-02-20 15:53:42 -0300

    Tp Grupal Agregado

---
+++ 3. Sprint I/be_java_hisp_w25_g05/src/main/java/org/socialmeli/dto/response/UserDTO.java
@@ -0,0 +1,14 @@
+package org.socialmeli.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDTO {
+    Integer userId;
+    String userName;
+}
