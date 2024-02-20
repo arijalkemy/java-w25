eb620600bde43f6168f8ebc563ab112417906648
GonzaloVega-MELI <gonzaloariel.vega@mercadolibre.com>
2024-02-20 12:12:27 -0300

    Sprint I parte grupal terminado

---
+++ 3. Sprint I/be_java_hisp_w25_g04/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/repository/SellerRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.breakingbytes.be_java_hisp_w25_g04.repository;
+
+import com.breakingbytes.be_java_hisp_w25_g04.entity.Post;
+import com.breakingbytes.be_java_hisp_w25_g04.entity.Seller;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import com.breakingbytes.be_java_hisp_w25_g04.entity.User;
+import java.util.Optional;
+
+@Repository
+public class SellerRepositoryImpl implements ISellerRepository{
+    @Override
+    public List<Seller> getSellers() {
+        return DbMock.getInstance().getListOfSellers();
+    }
+
+    @Override
+    public void addFollower(Seller seller, User follower) {
+        seller.addFollower(follower);
+    }
+  
+    public Optional<Seller> findById(int sellerId) {
+        return DbMock.getInstance().getListOfSellers().stream().filter(s -> s.getId() == sellerId).findFirst();
+    }
+
+    @Override
+    public void addPost(Post post, Seller seller) {
+        seller.getPosts().add(post);
+        DbMock.getInstance().getListOfPost().add(post);
+        DbMock.getInstance().getListOfProduct().add(post.getProduct());
+    }
+
+    @Override
+    public void setSellerFollowers(Integer sellerId, List<User> sellerFollowers) {
+        DbMock
+            .getInstance()
+            .getListOfSellers()
+            .stream()
+            .filter(s -> s.getId() == sellerId)
+            .findFirst()
+            .get()
+            .setFollowers(sellerFollowers);
+    }
+}
---
+++ 3. Sprint I/be_java_hisp_w25_g04/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/repository/UserRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.breakingbytes.be_java_hisp_w25_g04.repository;
+
+import com.breakingbytes.be_java_hisp_w25_g04.entity.Seller;
+import com.breakingbytes.be_java_hisp_w25_g04.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserRepositoryImpl implements IUserRepository{
+
+    @Override
+    public void setUserFollowings(Integer userId, List<Seller> userFollowings) {
+        DbMock
+            .getInstance()
+            .getListOfUsers()
+            .stream()
+            .filter(u -> u.getId() == userId)
+            .findFirst()
+            .get()
+            .setFollowing(userFollowings);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return DbMock.getInstance().getListOfUsers();
+    }
+
+
+    @Override
+    public Optional<User> findById(int userId) {
+        return DbMock
+                .getInstance()
+                .getListOfUsers()
+                .stream()
+                .filter(u -> u.getId() == userId)
+                .findFirst();
+    }
+
+    @Override
+    public void addFollowing(User user, Seller following) {
+        user.addFollowing(following);
+    }
+
+}
