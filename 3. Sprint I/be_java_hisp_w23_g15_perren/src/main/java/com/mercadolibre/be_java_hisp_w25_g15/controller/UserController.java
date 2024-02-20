a6a7111e30087626f04a2cc9e15fe668c936ab29
Martin Sergio Perren <martin.perren@mercadolibre.com>
2024-02-20 14:58:33 -0300

    Cambios a coleccion postman

--- 3. Sprint I/be_java_hisp_w23_g15_perren/src/main/java/com/mercadolibre/be_java_hisp_w25_g15/controller/UserController.java
+++ 3. Sprint I/be_java_hisp_w23_g15_perren/src/main/java/com/mercadolibre/be_java_hisp_w25_g15/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final IUserService userService;
 
