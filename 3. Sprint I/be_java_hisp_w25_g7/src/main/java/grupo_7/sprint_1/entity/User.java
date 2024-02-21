786248d80d96826cd47de0c572729085fec07b25
Francisco Lopez <franslopezcortes@gmail.com>
2024-02-21 15:31:38 -0300

    proyecto grupal

---
+++ 3. Sprint I/be_java_hisp_w25_g7/src/main/java/grupo_7/sprint_1/entity/User.java
@@ -0,0 +1,16 @@
+package grupo_7.sprint_1.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public abstract class User {
+    Integer userId;
+    String userName;
+}
---
+++ 3. Sprint I/be_java_hisp_w25_g7/src/test/java/grupo_7/sprint_1/Sprint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo_7.sprint_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
