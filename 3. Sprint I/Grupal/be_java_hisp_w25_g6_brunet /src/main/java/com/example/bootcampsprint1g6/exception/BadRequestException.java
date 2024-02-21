c9dac5b196335f959a9de0cc3bbb6a3d40f5b229
sabrunet <santiago.brunet@mercadolibre.com>
2024-02-21 13:34:10 -0300

    Ejercicio Grupo 6(Delete actualizacion)

--- 3. Sprint I/Grupal/be_java_hisp_w25_g6_brunet /src/main/java/com/example/bootcampsprint1g6/exception/BadRequestException.java
+++
@@ -1,5 +0,0 @@
-package com.example.bootcampsprint1g6.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) { super(message); }
-}
--- 3. Sprint I/Grupal/be_java_hisp_w25_g6_brunet /src/main/java/com/example/bootcampsprint1g6/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.bootcampsprint1g6.exception;
-
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
