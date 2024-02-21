a5e035d586467ce456980a1945c28edb918c5bc3
ndegutierrez <anderson.gutierrez@mercadolibre.com.co>
2024-02-21 14:02:01 -0500

    gutierrez_anderson

---
+++ 3. Sprint I/be_java_hisp_w25_g12/src/test/java/org/bootcamp/javazoo/JavazooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bootcamp.javazoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavazooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
