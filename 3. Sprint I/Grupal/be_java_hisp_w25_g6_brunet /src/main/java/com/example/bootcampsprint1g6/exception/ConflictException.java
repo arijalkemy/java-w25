51cf4792287b6a758ec8c9077215d54a6711878b
sabrunet <santiago.brunet@mercadolibre.com>
2024-02-21 12:24:32 -0300

    Ejercicio Grupo 6(Delete actualizacion)

--- 3. Sprint I/Grupal/be_java_hisp_w25_g6_brunet /src/main/java/com/example/bootcampsprint1g6/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bootcampsprint1g6.exception;
-
-public class ConflictException extends RuntimeException{
-    public ConflictException(String message) {
-        super(message);
-    }
-}
