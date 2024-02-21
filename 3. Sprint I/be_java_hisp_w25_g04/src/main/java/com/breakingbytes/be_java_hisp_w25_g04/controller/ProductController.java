0b424956bbbe3f3e5d93284c8e0cc806aaa9b066
GonzaloVega-MELI <gonzaloariel.vega@mercadolibre.com>
2024-02-21 14:50:14 -0300

    se agrega nueva entrega de sprint grupal con correcciones

---
+++ 3. Sprint I/be_java_hisp_w25_g04/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.breakingbytes.be_java_hisp_w25_g04.controller;
+
+import com.breakingbytes.be_java_hisp_w25_g04.dto.request.RequestPostDTO;
+import com.breakingbytes.be_java_hisp_w25_g04.dto.response.LastPostsDTO;
+import com.breakingbytes.be_java_hisp_w25_g04.service.ISellerService;
+import com.breakingbytes.be_java_hisp_w25_g04.service.IUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    ISellerService sellerService;
+    public ProductController(ISellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<LastPostsDTO> getPostOfVendorsFollowedByUser(
+            @PathVariable int userId,
+            @RequestParam(name = "order", required = false, defaultValue = "none") String order){
+        return ResponseEntity.ok()
+                .body(this.sellerService.getPostOfVendorsFollowedByUser(userId, order));
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> addPost(@RequestBody RequestPostDTO requestPostDTO){
+        sellerService.addPost(requestPostDTO);
+        return ResponseEntity
+                .ok().build();
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<?> getAllPosts(){
+        return ResponseEntity.ok()
+                .body(sellerService.findAllPosts());
+    }
+}
