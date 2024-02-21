7f3c23a1734fb84aff55e61f535cd0696912a940
toalmada_meli <tomasfrancisco.almada@mercadolibre.com>
2024-02-21 12:04:27 -0300

    Cambios formato fecha

--- 3. Sprint I/be_java_hisp_w25_g07_almada/src/main/java/grupo_7/sprint_1/dtos/PromoPostDto.java
+++ 3. Sprint I/be_java_hisp_w25_g07_almada/src/main/java/grupo_7/sprint_1/dtos/PromoPostDto.java
@@ -1,7 +1,9 @@
 package grupo_7.sprint_1.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.NonNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -16,9 +18,9 @@ public record PromoPostDto (
     @NonNull
     Double price,
     @NonNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     LocalDate date,
     @JsonProperty("has_promo")
-
     @NonNull
     Boolean hasPromo,
     @NonNull
