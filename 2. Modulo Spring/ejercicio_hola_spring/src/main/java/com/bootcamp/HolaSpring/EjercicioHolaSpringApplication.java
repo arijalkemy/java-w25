fb227e70f3965a20aab058fdbf3929c65be07e65
Tobias Medina Viegas <tobias.medinaviegas@mercadolibre.com>
2024-02-21 17:33:35 -0300

    Primeros ejercicios modulo Spring

---
+++ 2. Modulo Spring/ejercicio_hola_spring/src/main/java/com/bootcamp/HolaSpring/EjercicioHolaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.HolaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioHolaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioHolaSpringApplication.class, args);
+	}
+
+}
