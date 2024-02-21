6c0feece539856086ce9a829ee015f8b5fd7268b
Sergio Gabriel Mancilla Agreda <you@example.com>
2024-02-20 22:26:17 -0300

    grupal

---
+++ 3. Sprint I/be_java_hisp_w25_g07/src/main/java/grupo_7/sprint_1/repository/SellerRepositoryImp.java
@@ -0,0 +1,76 @@
+package grupo_7.sprint_1.repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import grupo_7.sprint_1.entity.Seller;
+import grupo_7.sprint_1.utils.Mapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class SellerRepositoryImp implements ISellerRepository {
+
+    Mapper mapper;
+    private List<Seller> sellers = new ArrayList<>();
+
+    public SellerRepositoryImp(Mapper mapper) throws IOException {
+        this.mapper = mapper;
+        loadSellers();
+    }
+
+    @Override
+    public void updateSeller(Seller seller) {
+        sellers.remove(seller);
+        sellers.add(seller);
+    }
+
+    @Override
+    public List<Seller> getAllSellers() {
+        return sellers;
+    }
+
+    public Optional<Seller> findById(Integer userId) {
+        return sellers.stream()
+                .filter(seller -> seller.getUserId().equals(userId))
+                .findFirst();
+    }
+
+    public int cantidadDeSeguidores(int userId) {
+        return sellers.stream()
+                .filter(seller -> seller.getUserId() == userId)
+                .findFirst()
+                .map(seller -> seller.getFollowers().size())
+                .orElse(0);
+    }
+
+    private void loadSellers() throws IOException {
+        File file;
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Seller> mappedSellers;
+
+        try {
+            file = ResourceUtils.getFile("classpath:sellers.json");
+
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            objectMapper.setDateFormat(new SimpleDateFormat("dd-MM-yyyy"));
+            mappedSellers = objectMapper.readValue(file, new TypeReference<List<Seller>>() {
+            });
+            sellers = mappedSellers;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println(LocalDate.now());
+        }
+    }
+}
+
---
+++ 3. Sprint I/be_java_hisp_w25_g07/src/main/java/grupo_7/sprint_1/service/ISellerService.java
@@ -0,0 +1,21 @@
+package grupo_7.sprint_1.service;
+
+import grupo_7.sprint_1.dtos.AddPostDto;
+import grupo_7.sprint_1.dtos.PostDto;
+import grupo_7.sprint_1.dtos.SellerDTO;
+import grupo_7.sprint_1.dtos.SellerFollowersListDto;
+import grupo_7.sprint_1.entity.Seller;
+
+import java.util.List;
+
+public interface ISellerService {
+    PostDto addPost(Integer sellerId, AddPostDto newPost);
+
+    SellerFollowersListDto getListOrderedAlphabetically(Integer userId, String order);
+
+    SellerDTO cantidadSeguidores(int userID);
+
+    List<PostDto> getRecentPostsFromFollowedSellers(Integer buyerId, String order);
+
+    List<Seller> getAllSellers();
+}
