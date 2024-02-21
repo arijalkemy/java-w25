aa8a41b516ee62c54c96387246f8476791dbccad
Gustavo-meli <gustavojavier.ferreyra@mercadolibre.com>
2024-02-21 13:00:35 -0300

    Actualizando sprint 1

--- 3. Sprint I/be_java_hisp_w25_g04/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.breakingbytes.be_java_hisp_w25_g04.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- 3. Sprint I/be_java_hisp_w25_g04/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/repository/IPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.breakingbytes.be_java_hisp_w25_g04.repository;
-
-import com.breakingbytes.be_java_hisp_w25_g04.entity.Post;
-
-import java.util.List;
-
-public interface IPostRepository {
-    List<Post> findAll();
-}
