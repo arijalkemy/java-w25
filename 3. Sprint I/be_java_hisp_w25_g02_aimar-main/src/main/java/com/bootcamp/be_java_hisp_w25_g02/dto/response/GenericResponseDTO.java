f7571829402bb59439a96d3e2cae642dba17cf6f
nico-aimar <mario.aimar@mercadolibre.com>
2024-02-21 17:00:12 -0300

    Sprint I - Desarrollo Individual

--- 3. Sprint I/be_java_hisp_w25_g02_aimar-main/src/main/java/com/bootcamp/be_java_hisp_w25_g02/dto/response/GenericResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package com.bootcamp.be_java_hisp_w25_g02.dto.response;
-
-public record GenericResponseDTO(String message) {
-
-}
--- 3. Sprint I/be_java_hisp_w25_g02_aimar-main/src/main/java/com/bootcamp/be_java_hisp_w25_g02/dto/response/UserDTO.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.be_java_hisp_w25_g02.dto.response;
-
-
-public record UserDTO(
-        Integer user_id,
-        String user_name
-) {
-}
