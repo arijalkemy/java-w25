0c63bff5ade7bff48920f45b99d2c8808b345a9b
Santiago Jaramillo <santiago.jaramilloocampo@mercadolibre.com.co>
2024-02-26 10:38:09 -0500

    Testing: mockMVC para ObtenerDiploma y tests unitarios StarWars

--- 4. Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
+++ 4. Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
@@ -3,8 +3,10 @@ package com.meli.obtenerdiploma.controller;
 import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.service.IObtenerDiplomaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 
 @RestController
 public class ObtenerDiplomaController {
@@ -16,5 +18,4 @@ public class ObtenerDiplomaController {
     public StudentDTO analyzeScores(@PathVariable Long studentId) {
         return service.analyzeScores(studentId);
     }
-
 }
---
+++ 4. Testing/starwars 2/src/test/java/com/mercadolibre/starwars/repositories/CharacterRepositoryImplTest.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.starwars.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterRepositoryImplTest {
+
+    @Test
+    void findAllByNameContains() {
+    }
+}
