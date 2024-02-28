a9a706735e0a8aa2df27ea3c14837ebb80c24525
Arrigoz <arrigo.zanabonidibatista@mercadolibre.com>
2024-02-28 16:49:05 -0300

    Proyecto testing individual

---
+++ 5. Sprint II/be_java_hisp_w25_g02_zanaboni/src/main/java/com/bootcamp/be_java_hisp_w25_g02/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.bootcamp.be_java_hisp_w25_g02.controller;
+import com.bootcamp.be_java_hisp_w25_g02.dto.request.PostDTO;
+import com.bootcamp.be_java_hisp_w25_g02.service.IPostService;
+import com.bootcamp.be_java_hisp_w25_g02.service.PostServiceImpl;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class ProductController {
+
+    private IPostService postService;
+    public ProductController(PostServiceImpl postService){
+
+        this.postService = postService;
+    }
+  
+    @PostMapping("products/post")
+    public ResponseEntity<?> savePost(@RequestBody @Valid PostDTO post) {
+        return new ResponseEntity<>(this.postService.savePost(post), HttpStatus.OK);
+    }
+
+    @GetMapping("products/followed/{userId}/list")
+    public ResponseEntity<?> getFollowedPosts(@PathVariable @Positive @NotNull Integer userId, @RequestParam( required = false) String order){
+        return new ResponseEntity<>(this.postService.getPostsOrderedByDate(userId, order), HttpStatus.OK);
+    }
+}
