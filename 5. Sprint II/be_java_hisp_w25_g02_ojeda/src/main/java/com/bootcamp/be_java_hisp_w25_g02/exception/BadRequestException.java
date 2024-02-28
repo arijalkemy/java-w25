4b51894a3378700f9331cbd67bc29609aa63f674
Guillermo Ojeda <guillermofrancisco.ojeda@mercadolibre.com>
2024-02-28 15:39:04 -0300

    Agregado Ejercicio Individual Sprint2 no completo

---
+++ 5. Sprint II/be_java_hisp_w25_g02_ojeda/src/main/java/com/bootcamp/be_java_hisp_w25_g02/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.bootcamp.be_java_hisp_w25_g02.exception;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message){
+        super(message);
+    }
+}
