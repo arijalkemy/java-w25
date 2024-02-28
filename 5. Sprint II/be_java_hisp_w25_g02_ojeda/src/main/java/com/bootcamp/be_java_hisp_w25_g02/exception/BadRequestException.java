aab87a078adfd8d0062a355dc48e2a4570bce3f4
Guillermo Ojeda <guillermofrancisco.ojeda@mercadolibre.com>
2024-02-28 16:34:58 -0300

    erased incorrect folder

--- 5. Sprint II/be_java_hisp_w25_g02_ojeda/src/main/java/com/bootcamp/be_java_hisp_w25_g02/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.be_java_hisp_w25_g02.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message){
-        super(message);
-    }
-}
