e8e7028c6a13217725adf39696c52b1219e1d6d4
Fabian-Rodriguez <fabianstiven201@gmail.com>
2024-02-29 18:12:25 -0500

    Spring 2 general

---
+++ 5. Sprint II/be_java_hisp_w25_g08/src/main/java/com/grupo08/socialmeli/dto/response/FollowDto.java
@@ -0,0 +1,25 @@
+package com.grupo08.socialmeli.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class FollowDto {
+    @JsonProperty("user_id")
+    @NotBlank(message = "El id no puede estar vacio.")
+    @Positive(message = "El id debe ser mayor a 0.")
+    public Integer userId;
+
+    @JsonProperty("user_name")
+    @Size(max = 15, message = "Maximo 15 caracteres")
+    public String userName;
+}
