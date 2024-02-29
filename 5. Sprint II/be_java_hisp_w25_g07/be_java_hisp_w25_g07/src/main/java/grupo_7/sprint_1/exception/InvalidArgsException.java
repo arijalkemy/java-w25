d47d21d6fd8fc07a5fc510ad7a683f0c180ea0c0
ITulian <ignacio.tulian@mercadolibre.com>
2024-02-29 09:09:17 -0300

    sprint 2 grupal

---
+++ 5. Sprint II/be_java_hisp_w25_g07/be_java_hisp_w25_g07/src/main/java/grupo_7/sprint_1/exception/InvalidArgsException.java
@@ -0,0 +1,7 @@
+package grupo_7.sprint_1.exception;
+
+public class InvalidArgsException extends RuntimeException {
+    public InvalidArgsException(String message) {
+        super(message);
+    }
+}
---
+++ 5. Sprint II/be_java_hisp_w25_g07/be_java_hisp_w25_g07/src/main/java/grupo_7/sprint_1/service/ISellerService.java
@@ -0,0 +1,20 @@
+package grupo_7.sprint_1.service;
+
+import grupo_7.sprint_1.dtos.AddPostDto;
+import grupo_7.sprint_1.dtos.PostDto;
+import grupo_7.sprint_1.dtos.SellerDTO;
+import grupo_7.sprint_1.dtos.SellerFollowersListDto;
+
+import java.util.List;
+
+public interface ISellerService {
+    PostDto addPost(Integer sellerId, AddPostDto newPost);
+
+    SellerFollowersListDto getListOrderedAlphabetically(Integer userId, String order);
+
+    SellerDTO cantidadSeguidores(int userID);
+
+    List<PostDto> getRecentPostsFromFollowedSellers(Integer buyerId, String order);
+
+
+}
