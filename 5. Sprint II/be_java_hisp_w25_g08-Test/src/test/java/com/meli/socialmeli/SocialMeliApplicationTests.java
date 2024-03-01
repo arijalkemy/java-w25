e7651893eeaf2b8af1958c556caa4b2745d8e6d6
Carlos Enrique Luna Garcia <57882278+carlosLunaGarca@users.noreply.github.com>
2024-03-01 01:12:09 -0500

    Commit sprint II

---
+++ 5. Sprint II/be_java_hisp_w25_g08-Test/src/test/java/com/meli/socialmeli/SocialMeliApplicationTests.java
@@ -0,0 +1,14 @@
+package com.meli.socialmeli;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class SocialMeliApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
