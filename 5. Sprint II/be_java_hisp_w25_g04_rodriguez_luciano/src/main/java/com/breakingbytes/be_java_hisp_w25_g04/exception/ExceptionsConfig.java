81c52214476731cbd5ab6670551c0d35062b56d4
lrodriguezlo <luciano.rodriguezloza@mercadolibre.com>
2024-03-01 10:52:09 -0300

    agrego control de excepciones para validaciones

--- 5. Sprint II/be_java_hisp_w25_g04_rodriguez_luciano/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/ExceptionsConfig.java
+++ 5. Sprint II/be_java_hisp_w25_g04_rodriguez_luciano/src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/ExceptionsConfig.java
@@ -3,6 +3,9 @@ package com.breakingbytes.be_java_hisp_w25_g04.exception;
 import com.breakingbytes.be_java_hisp_w25_g04.dto.response.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,5 +22,23 @@ public class ExceptionsConfig {
     public ResponseEntity<ExceptionDTO> badRequest(BadRequestException e) {
         ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> notValid(MethodArgumentNotValidException e){
+        ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDto,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> notValid(HttpMessageNotReadableException e){
+        ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDto,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> notValid(MissingServletRequestParameterException e){
+        ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDto,HttpStatus.BAD_REQUEST);
     }
 }
