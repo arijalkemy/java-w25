4a129228b10c2563a26e78cdcbb9fbbe1d5bc86b
afmadera <112901535+afmadera@users.noreply.github.com>
2024-03-01 10:36:55 -0500

    Eliminar

--- 5. Sprint II/be_java_hisp_w25_g08_madera/src/main/java/com/grupo08/socialmeli/exception/BadRequestException.java
+++
@@ -1,5 +0,0 @@
-package com.grupo08.socialmeli.exception;
-
-public class BadRequestException  extends RuntimeException{
-    public BadRequestException(String message){ super(message); }
-}
--- 5. Sprint II/be_java_hisp_w25_g08_madera/src/main/java/com/grupo08/socialmeli/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.grupo08.socialmeli.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}
