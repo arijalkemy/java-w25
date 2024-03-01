369d1b80415029ba4155416a4559828a7edaa642
Vanessa Alexandra Gonzalez Casallas <vanessa.gonzalezcasallas@mercadolibre.com.co>
2024-03-01 10:50:05 -0500

    .

--- 5. Sprint II/be_java_hisp_w25_g10_Gonzalez/src/main/java/com/example/be_java_hisp_w25_g10/exceptions/InvalidRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.example.be_java_hisp_w25_g10.exceptions;
-
-public class InvalidRequestException extends RuntimeException{
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-
-}
