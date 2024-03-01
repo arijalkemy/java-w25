197edde296d68f70d6d999de60a3f1f983dbeadf
Sergio Gabriel Mancilla Agreda <you@example.com>
2024-03-01 14:16:07 -0300

    carpeta grupal

---
+++ 5. Sprint II/be_java_hisp_w25_g07/src/main/java/grupo_7/sprint_1/entity/Product.java
@@ -0,0 +1,22 @@
+package grupo_7.sprint_1.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class Product {
+    Integer productId;
+    String productName;
+    String type;
+    String brand;
+    String color;
+    String notes;
+
+    public Product(Integer productId) {
+        this.productId = productId;
+    }
+}
