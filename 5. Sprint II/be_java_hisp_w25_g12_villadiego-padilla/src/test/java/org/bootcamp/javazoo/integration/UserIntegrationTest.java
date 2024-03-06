ea232f8c1cb6180d0b9154ee4307f66d5f0e0c08
dvilladiegop <danielarosa.villadiegopadilla@mercadolibre.com.co>
2024-03-05 20:31:51 -0500

    Ejercicio de consulta a una base de datos

--- 5. Sprint II/be_java_hisp_w25_g12_villadiego-padilla/src/test/java/org/bootcamp/javazoo/integration/UserIntegrationTest.java
+++ 5. Sprint II/be_java_hisp_w25_g12_villadiego-padilla/src/test/java/org/bootcamp/javazoo/integration/UserIntegrationTest.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserIntegrationTest {
+public class UserIntegrationTest {a
 
     @Autowired
     MockMvc mockMvc;
