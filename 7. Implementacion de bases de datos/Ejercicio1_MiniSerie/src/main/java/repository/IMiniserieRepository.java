75d55fecb52c1b659fe588555d466a43b5c3217b
sabrunet <santiago.brunet@mercadolibre.com>
2024-03-11 10:53:13 -0300

    Ejercicio Implementacion BD MiniSerie

---
+++ 7. Implementacion de bases de datos/Ejercicio1_MiniSerie/src/main/java/repository/IMiniserieRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniserieRepository extends JpaRepository<MiniSerie, Long> {
+}
