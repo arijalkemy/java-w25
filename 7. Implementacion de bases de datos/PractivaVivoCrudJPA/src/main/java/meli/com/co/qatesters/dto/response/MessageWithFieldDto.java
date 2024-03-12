a56d362086257bc235150af51ffc5287298f51a6
JonathanPinilla <jonathan.pinillaforero@mercadolibre.com.co>
2024-03-12 08:02:14 -0500

    Practica Vivo CRUD JPAs

---
+++ 7. Implementacion de bases de datos/PractivaVivoCrudJPA/src/main/java/meli/com/co/qatesters/dto/response/MessageWithFieldDto.java
@@ -0,0 +1,4 @@
+package meli.com.co.qatesters.dto.response;
+
+public record MessageWithFieldDto(String field, String message) {
+}
---
+++ 7. Implementacion de bases de datos/PractivaVivoCrudJPA/src/test/java/meli/com/co/qatesters/QaTestersApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.com.co.qatesters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaTestersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
