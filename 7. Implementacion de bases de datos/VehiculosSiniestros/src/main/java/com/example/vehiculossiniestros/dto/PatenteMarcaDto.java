ebc989fd42fcdbc0b3f63026eeb743e6555f3a78
Fabian Stiven Rodriguez Quintero <52023793+Fabian-Rodriguez@users.noreply.github.com>
2024-03-12 12:28:31 -0500

    Implementacion bases de datos

---
+++ 7. Implementacion de bases de datos/VehiculosSiniestros/src/main/java/com/example/vehiculossiniestros/dto/PatenteMarcaDto.java
@@ -0,0 +1,9 @@
+package com.example.vehiculossiniestros.dto;
+
+import lombok.Data;
+
+@Data
+public class PatenteMarcaDto {
+  private String patente;
+  private String marca;
+}
---
+++ 7. Implementacion de bases de datos/VehiculosSiniestros/src/test/java/com/example/vehiculossiniestros/VehiculosSiniestrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehiculossiniestros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculosSiniestrosApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
